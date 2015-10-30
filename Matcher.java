b2960b8d196aabd204fdceeb9b5d2cf9522810fe
apraturu <apraturu@calpoly.edu>
2015-10-30 13:00:20 -0700

    JSON parsing done...converts to a person object

--- Matcher.java
+++ Matcher.java
@@ -3,8 +3,18 @@
  * algorithm and test driver.
  */
 
+import java.io.*;
+import com.google.gson.*;
+
 public class Matcher {
-   public static void main(String[] args) {
-      
+   public static void main(String[] args) throws IOException {
+      Gson gson = new Gson();
+      BufferedReader br1 = new BufferedReader(new FileReader("ckmccann.json"));
+      ParsedData data1 = gson.fromJson(br1, ParsedData.class);
+      BufferedReader br2 = new BufferedReader(new FileReader("eschen.json"));
+      ParsedData data2 = gson.fromJson(br2, ParsedData.class);
+
+      Person p1 = data1.createPerson();
+      Person p1 = data2.createPerson();
    }
 }
---
+++ ParsedData.java
@@ -0,0 +1,25 @@
+public class ParsedData {
+
+   private String FirstName;
+   private String LastName;
+   private String Email;
+   private String Gender;
+   private String Major;
+   private String Religion;
+   private boolean Substances;
+   private int Noise;
+   private String Organizations;
+   private int SleepTime;
+   private int Cleanliness;
+
+   public Person createPerson() {
+      return new Person(FirstName, LastName, Email, Gender, Major, Religion,
+       Substances, Noise, Cleanliness, Organizations, SleepTime);
+   }
+
+   @Override
+   public String toString() {
+      return "DataObject [fn=" + FirstName + ", ln=" + LastName + ", email="
+       + Email + "]";
+   }
+}
