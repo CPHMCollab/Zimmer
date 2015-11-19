dc99a28047f44b452e5eb4e3c451d5239c8b6a75
krcornel <krcornel@calpoly.edu>
2015-11-18 18:24:42 -0800

    Update Criteria.java

--- Criteria.java
+++ Criteria.java
@@ -5,8 +5,14 @@
 public Criteria {
     private String name;
     private int weight;
+    private int score;
+    
+    public Criteria()
     
     public String getName() { return this.name; }
-    
-    public boolean equals(Object o);
+    public void setName(String name) { this.name = name; }
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
+    public int getWeight() { return weight; }
+    public void setWeight(int weight) { this.weight = weight; }
 }
