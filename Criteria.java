d547e0f9135e4fd26e50714d789f9612832f7cc4
apraturu <apraturu@calpoly.edu>
2015-11-23 16:40:18 -0800

    update to include expected

--- Criteria.java
+++ Criteria.java
@@ -13,18 +13,23 @@ public Criteria {
     private double percentageWeight;
     /** A person's self-evaluation of their criteria as a roommate. */
     private int score;
+    /** A person's expectation for this criteria in a roommate */
+    private int expected;
     
-    public Criteria(String name, double weight, int score)
+    public Criteria(String name, double weight, int score, int expected)
     {
         this.name = name;
         this.percentageWeight = weight;
         this.score = score;
+        this.expected = expected;
     }
     
     public String getName() { return this.name; }
     public void setName(String name) { this.name = name; }
     public int getScore() { return score; }
     public void setScore(int score) { this.score = score; }
+    public int getExpected() { return expected; }
+    public void setExpected(int expected) { this.expected = expected; }
     public double getPercentageWeight() { return percentageWeight; }
     public void setPercentageWeight(double weight) { this.percentageWeight = weight; }
 }
